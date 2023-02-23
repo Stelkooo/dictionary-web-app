6c05ab2d3ff2c3381c8d27d4653b31c8805ff4e8
stelko <stilyanmtoskov@gmail.com>
2023-02-23 23:28:46 +0000

    search bar empty warning

--- src/components/search-bar/search-bar.component.jsx
+++ src/components/search-bar/search-bar.component.jsx
@@ -4,19 +4,24 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setSearch } from '../../store/search/search.reducer';
 
 // import selector
-import { selectSearchQuery } from '../../store/search/search.selector';
+import {
+  selectSearchQuery,
+  selectHasUserSearched,
+} from '../../store/search/search.selector';
 
 // style import
 import {
   SearchBarContainer,
   SearchBarInput,
   SearchIcon,
+  Error,
 } from './search-bar.styles';
 
 function SearchBar() {
   const dispatch = useDispatch();
 
   const searchValue = useSelector(selectSearchQuery);
+  const hasUserSearched = useSelector(selectHasUserSearched);
 
   const onChangeHandler = (value) => {
     dispatch(setSearch(value));
@@ -25,11 +30,15 @@ function SearchBar() {
   return (
     <SearchBarContainer>
       <SearchBarInput
+        $displayError={!searchValue && hasUserSearched}
         type="text"
         placeholder="Search for any word..."
         value={searchValue}
         onChange={(e) => onChangeHandler(e.currentTarget.value)}
       />
+      {!searchValue && hasUserSearched ? (
+        <Error>Whoops, can&apos;t be empty...</Error>
+      ) : null}
       <SearchIcon />
     </SearchBarContainer>
   );
--- src/components/search-bar/search-bar.styles.jsx
+++ src/components/search-bar/search-bar.styles.jsx
@@ -18,14 +18,19 @@ export const SearchBarInput = styled.input`
   height: ${48 / 16}rem;
   border-radius: 1rem;
   background-color: ${({ theme }) => theme.theme.searchBar};
-  border: none;
+  border: ${(props) =>
+    props.$displayError ? `1px solid ${props.theme.colours.warning}` : 'none'};
   outline: none;
   font-weight: 700;
   caret-color: ${({ theme }) => theme.colours.purple};
 
   &:focus {
-    border: 1px solid ${({ theme }) => theme.colours.purple};
+    border: ${(props) =>
+      props.$displayError
+        ? `1px solid ${props.theme.colours.warning}`
+        : `1px solid ${props.theme.colours.purple}`};
   }
+
   @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
     padding: ${24 / 16}rem;
     height: ${64 / 16}rem;
@@ -37,4 +42,12 @@ export const SearchIcon = styled(SearchSvg)`
   position: absolute;
   top: ${24 / 16}rem;
   right: ${24 / 16}rem;
+`;
+
+export const Error = styled.p`
+  margin-top: 1rem;
+  color: ${({ theme }) => theme.colours.warning};
+  @media screen and (min-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: ${({ theme }) => theme.fontSizes.headingSmall};
+  }
 `;
